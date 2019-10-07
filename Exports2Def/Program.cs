147dd67bed9c1d4b7baaa1be2f785c05898d74e4
José Ángel Martínez Torres <joseangelmt@outlook.com>
2019-10-07 10:43:16 +0200

    Primer commit

--- Exports2Def/Program.cs
+++ Exports2Def/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
 
 namespace Exports2Def
 {
@@ -9,6 +8,29 @@ namespace Exports2Def
     {
         static void Main(string[] args)
         {
+            if( args.Length < 2 )
+            {
+                Console.Error.WriteLine("Error, no se han especificado suficientes parámetros.");
+                return;
+            }
+
+            var i = 0;
+            var líneas = File.ReadAllLines(args[0]);
+            using(var archivo = new StreamWriter(args[1]))
+            {
+                archivo.WriteLine($";{args[1]}: Creado automáticamente por Exports2Def");
+                archivo.WriteLine();
+                archivo.WriteLine("EXPORTS");
+
+                foreach(var línea in líneas.Skip(16))
+                {
+                    var palabras = línea.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                    if (palabras.Length < 4)
+                        continue;
+                    archivo.WriteLine($"{palabras[3]}\t@{++i}\tNONAME");
+                }
+            }
         }
     }
 }
